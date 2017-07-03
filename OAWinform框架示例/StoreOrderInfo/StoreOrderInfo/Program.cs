6a94d87fbaeeb3deb830f5554522ea1d7cbd56ab
猪猪魅无影 <l>
2017-07-03 15:02:47 +0800

    First commit

---
+++ OAWinform框架示例/StoreOrderInfo/StoreOrderInfo/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using NetDimension.NanUI;
+
+namespace StoreOrderInfo
+{
+    static class Program
+    {
+        /// <summary>
+        /// 应用程序的主入口点。
+        /// </summary>
+        [STAThread]
+        static void Main()
+        {
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);
+            HtmlUILauncher.EnableFlashSupport = true;
+            if (HtmlUILauncher.InitializeChromium((args => {
+                args.Settings.LogSeverity = Chromium.CfxLogSeverity.Default;
+            })))
+            {
+                HtmlUILauncher.RegisterEmbeddedScheme(System.Reflection.Assembly.GetExecutingAssembly());
+                Application.Run(new UI());
+            }
+        }
+    }
+}
---
+++ OAWinform框架示例/StoreOrderInfo/StoreOrderInfo/UI.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using NetDimension.NanUI;
+
+namespace StoreOrderInfo
+{
+    public partial class UI : HtmlUIForm
+    {
+        public UI() : base("embedded://Code/index.html")
+        {
+            InitializeComponent();
+           
+        }
+    }
+}
+// GlobalObject.Add("hostEditor", new JsCodeEditorObject(this));
+//private void JsCodeEditorObject_ExecuteSetClean(object sender, Chromium.Remote.Event.CfrV8HandlerExecuteEventArgs e)
+//{
+//    if (e.Arguments.Length > 0)
+//    {
+//        parentForm.isClean = e.Arguments.First(p => p.IsBool).BoolValue;
+//    }
+//}
